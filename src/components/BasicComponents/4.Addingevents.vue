2ea1bb647dd46953845357e99d6a5efe87231088
mushex avetisyan <avetisyan.mushex@inbox.ru>
2022-04-04 00:32:02 +0400

    asee

--- src/components/BasicComponents/4.Addingevents.vue
+++ src/components/BasicComponents/4.Addingevents.vue
@@ -2,7 +2,12 @@
   <div>
     <h1>Adding Events</h1>
     <div class="main_content_two">
+      <h2>Counter {{ count }}</h2>
+      <button v-on:click="riseCounter">Add</button>
 
+      <hr>
+      <h2 v-on:mouseover="onHover">Counter 2 {{ count2 }}</h2>
+      <button v-on:click="count2++">Add</button>
     </div>
   </div>
 </template>
@@ -10,14 +15,26 @@
 <script>
 export default {
   data: () => ({
-
-  })
+    count: 0,
+    count2: 0
+  }),
+  methods: {
+    riseCounter: function() {
+      this.count++
+    },
+    onHover: function(event) {
+      event.target.style.color = 'red'
+    }
+  }
 }
 </script>
 
 <style scoped lang="scss">
 h1{
   color: white;
+}
+button{
+  cursor: pointer;
 }
 .main_content_two{
   p{
