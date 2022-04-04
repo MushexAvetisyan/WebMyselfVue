780dcdcf080b3e8fef35308a82279452befd070e
MushexAvetisyan <avetisyan.mushex@inbox.ru>
2022-04-04 13:45:32 +0400

    asdasd

--- src/components/BasicComponents/5.PassingParameterstoaMethod.vue
+++ src/components/BasicComponents/5.PassingParameterstoaMethod.vue
@@ -1,7 +1,10 @@
 <template>
   <div>
+    <h1>Passing Parameters To Method</h1>
     <div class="main_content_two">
-      <p>Dynamic Attributes</p>
+      <h2>Counter {{ count }}</h2>
+      <button v-on:click="riseCounter(5)">Add + 5 </button>
+      <button v-on:click="riseCounter(10)">Add + 10 </button>
     </div>
   </div>
 </template>
@@ -9,12 +12,20 @@
 <script>
 export default {
   data: () => ({
-
-  })
+    count: 0,
+  }),
+  methods: {
+    riseCounter: function(num) {
+      this.count += num
+    },
+  }
 }
 </script>
 
 <style scoped lang="scss">
+h1, h2{
+  color: white;
+}
 .main_content_two{
   p{
     color: white;
