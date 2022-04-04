227e0a7d5077cea96db4e9a2019ea385db61a439
mushex avetisyan <avetisyan.mushex@inbox.ru>
2022-04-04 23:54:45 +0400

    Events Modifier

--- src/components/BasicComponents/5.PassingParameterstoaMethod.vue
+++ src/components/BasicComponents/5.PassingParameterstoaMethod.vue
@@ -2,9 +2,9 @@
   <div>
     <h1>Passing Parameters To Method</h1>
     <div class="main_content_two">
-      <h2>Counter {{ count }}</h2>
-      <button v-on:click="riseCounter(5)">Add + 5 </button>
-      <button v-on:click="riseCounter(10)">Add + 10 </button>
+      <h2>{{ title }} = {{ count }}</h2>
+      <button v-on:click="riseCounter(5, 'changing on 5', $event)">Add + 5 </button>
+      <button v-on:click="riseCounter(10, 'changing on 10', $event)">Add + 10 </button>
     </div>
   </div>
 </template>
@@ -12,11 +12,20 @@
 <script>
 export default {
   data: () => ({
+    title: "Counter",
     count: 0,
   }),
   methods: {
-    riseCounter: function(num) {
+    riseCounter: function(num, str, event) {
       this.count += num
+      this.title = str
+
+      if (num === 5 ) {
+        event.target.style.color = 'blue'
+      }
+      else if (num === 10) {
+        event.target.style.color = 'red'
+      }
     },
   }
 }
--- src/components/BasicComponents/6.Eventmodifiers.vue
+++ src/components/BasicComponents/6.Eventmodifiers.vue
@@ -1,7 +1,16 @@
 <template>
   <div>
+    <h1>Event Modifier</h1>
     <div class="main_content_two">
-      <p>Dynamic Attributes</p>
+      <h2>
+        <a href="https://google.ru/" target="_blank" v-on:click.prevent="">Google</a>
+      </h2>
+
+      <h2 v-on:mousemove="handleMouseMove" style="background-color: red">
+        X: {{ x }} \ Y: {{ y }}
+
+        <span v-on:mousemove.stop="" style="background-color: green">No Changing</span>
+      </h2>
     </div>
   </div>
 </template>
@@ -9,12 +18,22 @@
 <script>
 export default {
   data: () => ({
-
-  })
+    x: 0,
+    y: 0,
+  }),
+  methods: {
+    handleMouseMove: function(event) {
+      this.x = event.clientX
+      this.y = event.clientY
+    }
+  }
 }
 </script>
 
 <style scoped lang="scss">
+h1{
+  color: white;
+}
 .main_content_two{
   p{
     color: white;
