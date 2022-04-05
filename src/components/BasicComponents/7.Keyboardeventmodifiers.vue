71d1a71ea5651c961e024e16f931b483c725c5c3
MushexAvetisyan <avetisyan.mushex@inbox.ru>
2022-04-05 18:21:06 +0400

    asdasd

--- src/components/BasicComponents/7.Keyboardeventmodifiers.vue
+++ src/components/BasicComponents/7.Keyboardeventmodifiers.vue
@@ -1,7 +1,19 @@
 <template>
   <div>
+    <h1>Keyboards Events</h1>
     <div class="main_content_two">
-      <p>Dynamic Attributes</p>
+
+      <h2 v-on:mousemove="handleMouseMove" style="background-color: red">
+        X: {{ x }} \ Y: {{ y }}
+
+        <span v-on:mousemove.stop="" style="background-color: green">No Changing</span>
+      </h2>
+
+      <hr>
+
+      <input type="text" v-on:keyup.enter="alertValue">
+
+
     </div>
   </div>
 </template>
@@ -9,12 +21,25 @@
 <script>
 export default {
   data: () => ({
-
-  })
+    x: 0,
+    y: 0,
+  }),
+  methods: {
+    handleMouseMove: function(event) {
+      this.x = event.clientX
+      this.y = event.clientY
+    },
+    alertValue: function(event) {
+        alert(event.target.value)
+    }
+  }
 }
 </script>
 
 <style scoped lang="scss">
+h1{
+  color: white;
+}
 .main_content_two{
   p{
     color: white;
